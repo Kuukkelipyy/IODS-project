fa293820f20457d7fc990666feab6f497db01dae
Hans Hämäläinen <hamalainen.hans@gmail.comi>
2021-11-16 17:53:47 +0200

    Chp4 addons

---
+++ data/create_human.R
@@ -0,0 +1,18 @@
+################################################################################
+
+# Hans Hämäläinen
+# 16.11.2021
+
+# Introduction to Open Data Science 2021:
+## RStudio Exercise 4: Data wrangling
+
+## Data sets used here:
+### 1) Human development data
+### 2) Gender inequality data
+## Data source:  source: http://hdr.undp.org/en/content/human-development-index-hdi
+
+################################################################################
+
+library(dplyr) 
+
+################################################################################
