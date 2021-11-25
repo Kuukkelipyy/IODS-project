f4924d33a593214e1adb5a91d69dcbb8849c2489
Hans Hämäläinen <hamalainen.hans@gmail.comi>
2021-11-25 12:10:53 +0200

    Chapter 6: first commit

---
+++ data/meet_and_repeat.R
@@ -0,0 +1,39 @@
+################################################################################
+
+# Hans Hämäläinen
+# 25.11.2021
+
+# Introduction to Open Data Science 2021:
+## RStudio Exercise 6: Data wrangling
+
+## Data sets used here:
+### 1) 
+### 2) 
+## Source: https://github.com/KimmoVehkalahti/MABS  
+
+################################################################################
+
+library(dplyr) 
+
+################################################################################
+
+# download the data sets in wide format
+BPRS <- read.table("https://raw.githubusercontent.com/KimmoVehkalahti/MABS/master/Examples/data/BPRS.txt", header = TRUE)
+RATS <- read.table("https://raw.githubusercontent.com/KimmoVehkalahti/MABS/master/Examples/data/rats.txt", header = TRUE)
+
+
+
+
+
+
+
+
+
+
+
+
+################################################################################
+################################################################################
+######################### THAT'S ALL DOCS! #####################################
+################################################################################
+################################################################################
